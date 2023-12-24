c16b3e626226c9a1bb846ea75c881da257d75293
Jagdeesh Bhandari <jagdeeshbhandari05@gmail.com>
2023-12-24 07:43:23 +0530

    in counter js add a new event listner for if we chage the value of progress song duration also changes

--- counter.js
+++ counter.js
@@ -2,10 +2,14 @@ let progress = document.getElementById('progress');
 let song = document.getElementById('song');
 let ctr = document.getElementById('ctr');
 
+console.log(progress);
 
 song.onloadedmetadata=()=>{
+  console.log(song.duration);
+  console.log(song.currentTime);
+
   progress.max = song.duration;
-  progress.value = song.currenrTime;
+  progress.value = song.currentTime;
 }
 const playPause = ()=>{
   if(ctr.classList.contains("fa-pause")){
@@ -22,7 +26,12 @@ const playPause = ()=>{
 
 if(song.play()){
   setInterval(()=>{
-    progress.value = song.currenrTime;
-
+    progress.value = song.currentTime;
   },500)
 }
+
+progress.addEventListener("change",()=>{
+  console.log("hello");
+  
+  song.currentTime = progress.value;
+})
